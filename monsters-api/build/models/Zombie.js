c9bdc228e81e04c2e6c089a695f001807be8c7f2
Gloria H <silverdragonia@gmail.com>
2018-10-27 17:04:21 -0400

    initial commit

---
+++ monsters-api/build/models/Zombie.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Zombie {
+    // brainsEaten = 0;
+    // hunger = 50;
+    // weaknesses = [Weakness.Fire1, Weakness.Stake1];
+    constructor(name) {
+        this.name = name;
+    }
+}
+exports.default = Zombie;
