59a203d5ac7d7bcb649c3f015505e099764b6944
Gloria H <silverdragonia@gmail.com>
2018-10-29 15:07:24 -0400

    added /:name route, req working

--- monsters-api/app.ts
+++ monsters-api/app.ts
@@ -3,11 +3,16 @@ import * as express from 'express';
 // Models
 import Monster from './interfaces/Monster';
 import Vampire from './models/Vampire';
+import Zombie from './models/Zombie';
+import {Food} from './interfaces/enums';
+import {Attack} from './interfaces/enums';
+import {Weakness} from './interfaces/enums';
 
 const app = express();
 
 const myMonsters = {
   'Nosforatu': new Vampire('Nosforatu'),
+  'Joe': new Zombie('Joe'),
 }
 
 app.get('/', function(req, res) {
@@ -19,7 +24,12 @@ app.get('/roll-call', function(req, res){
   for (let monster in myMonsters) {
     rollCall.push((myMonsters[monster] as Monster).name);
   }
-  res.send(rollCall.join(','));
+  res.send(rollCall.join(', '));
+});
+
+app.get('/:name', function(req, res){
+  let name = req.params.name;
+  res.send(name);
 });
 
 app.listen('3000', function(){
