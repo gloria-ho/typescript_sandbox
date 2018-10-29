ddd87c0972babfd8439940cab3005309f562a7da
Gloria H <silverdragonia@gmail.com>
2018-10-29 15:07:47 -0400

    added /:name route, req working

--- monsters-api/build/app.js
+++ monsters-api/build/app.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const Vampire_1 = require("./models/Vampire");
+const Zombie_1 = require("./models/Zombie");
 const app = express();
 const myMonsters = {
     'Nosforatu': new Vampire_1.default('Nosforatu'),
+    'Joe': new Zombie_1.default('Joe'),
 };
 app.get('/', function (req, res) {
     res.send('Hello, this is a test that the server is running.');
@@ -14,7 +16,11 @@ app.get('/roll-call', function (req, res) {
     for (let monster in myMonsters) {
         rollCall.push(myMonsters[monster].name);
     }
-    res.send(rollCall.join(','));
+    res.send(rollCall.join(', '));
+});
+app.get('/:name', function (req, res) {
+    let name = req.params.name;
+    res.send(name);
 });
 app.listen('3000', function () {
     console.log('listening on 3000');
